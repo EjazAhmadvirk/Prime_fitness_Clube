21e285aa5014120c5f9c2686d38e30020c91dba5
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-07-30 09:19:10 +0500

    project files

---
+++ Project_files/src/JOptionpane.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MUGHAL TRADERS
+ */
+class JOptionpane {
+
+    static void showMessageDialog(Object object, String sUccessfully_Saved) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void showMessage(Object object, Exception e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
